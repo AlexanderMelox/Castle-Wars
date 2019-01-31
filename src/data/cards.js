7b273fcac484f2096ab475541e5367baf4cefe7e
Alexander Melo <alexander.melo@ibm.com>
2019-01-31 15:48:43 -0600

    first commit / added type:weapons cards

---
+++ src/data/cards.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    name: 'archer',
+    type: 'weapons',
+    cost: 2,
+    description: 'attack 2'
+  },
+  {
+    name: 'platoon',
+    type: 'weapons',
+    cost: 4,
+    description: 'attack 6'
+  },
+  {
+    name: 'banshee',
+    type: 'weapons',
+    cost: 24,
+    description: 'attack 32',
+  },
+  {
+    name: 'swat',
+    type: 'weapons',
+    cost: 18,
+    description: 'enemy castle 32',
+  },
+  {
+    name: 'thief',
+    type: 'weapons',
+    cost: 15,
+    description: 'transfer enemy stocks 5',
+  },
+  {
+    name: 'saboteur',
+    type: 'weapons',
+    cost: 12,
+    description: 'enemy stocks -4',
+  },
+  {
+    name: 'attack',
+    type: 'weapons',
+    cost: 10,
+    description: 'attack 12',
+  },
+  {
+    name: 'recruit',
+    type: 'weapons',
+    cost: 8,
+    description: 'soldiers 1',
+  },
+  {
+    name: 'rider',
+    type: 'weapons',
+    cost: 2,
+    description: 'attack 4',
+  },
+  {
+    name: 'knight',
+    type: 'weapons',
+    cost: 2,
+    description: 'attack 3',
+  },
+];
